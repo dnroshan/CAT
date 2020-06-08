dcc153a90bf085da1a4ab9ccda0eacbc663831de
dnxroshan <dilnavasroshan@gmail.com>
2020-06-08 11:22:02 +0530

    initial commit

---
+++ static/js/utils.js
@@ -0,0 +1,54 @@
+
+
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+  function checkCookie() {
+    var user = getCookie("username");
+    if (user != "") {
+      alert("Welcome again " + user);
+    } else {
+      user = prompt("Please enter your name:", "");
+      if (user != "" && user != null) {
+        setCookie("username", user, 365);
+      }
+    }
+  } 
+
+  function add_parm(parm, value){
+    history.pushState(null, null, window.location.href+'?'+parm+'='+value);
+  }
+
+  function get_parm(parm){
+    var url_string = window.location.href;
+    var url = new URL(url_string);
+    return url.searchParams.get(parm);
+  }
+
+  function change_parm(parm, value){
+    history.replaceState(null, null, '?'+parm+'='+value);
+  }
+
+  
